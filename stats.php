11d49d35788dd7876c41439087daf6014846db10
Will Presley (sonata) <william.presley.jr+git@gmail.com>
2024-10-18 10:43:17 -0400

    Fix up SQL query to make stats table work again

--- stats.php
+++ stats.php
@@ -81,20 +81,20 @@ SQL;
         }
 
         $sql = <<<SQL
-SELECT
-	feed.id,
-	feed.name,
-	feed.`lastUpdate`,
-	feed.ttl,
-	CASE WHEN COUNT(1) > 0 THEN ((MAX(stats.date) - MIN(stats.date)) / COUNT(1)) ELSE 0 END AS `avgTTL`,
-	MAX(stats.date) AS date_max
-FROM `_feed` AS feed
-LEFT JOIN `_entry` AS stats ON feed.id = stats.id_feed
-WHERE {$where}
-GROUP BY stats.id_feed
-ORDER BY `avgTTL` ASC
-LIMIT {$limit}
-SQL;
+        SELECT
+            feed.id AS feed_id,  -- Alias `feed.id` as `feed_id`
+            feed.name,
+            feed.`lastUpdate`,
+            feed.ttl,
+            CASE WHEN COUNT(stats.id_feed) > 0 THEN ((MAX(stats.date) - MIN(stats.date)) / COUNT(stats.id_feed)) ELSE 0 END AS `avgTTL`,
+            MAX(stats.date) AS date_max
+        FROM `_feed` AS feed
+        LEFT JOIN `_entry` AS stats ON feed.id = stats.id_feed
+        WHERE {$where}
+        GROUP BY feed.id
+        ORDER BY `avgTTL` ASC
+        LIMIT {$limit}
+        SQL;
 
         $stm = $this->pdo->query($sql);
         $res = $stm->fetchAll(PDO::FETCH_NAMED);
